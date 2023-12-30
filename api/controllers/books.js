195286528326c5ad739ec242ce4427b6803668a4
Mohd Abuzaid Ansari <mohdabuzaid15@gmail.com>
2023-12-30 16:47:17 +0530

    cloudinary

--- api/controllers/books.js
+++ api/controllers/books.js
@@ -1,12 +1,13 @@
 const books = require('../models/BookSchema');
-const multer=require('multer')
+const multer = require('multer')
 const path = require('path');
 const fs = require('fs');
+const cloudinary = require('../util/cloudinary');
 
 const getAllBooks = async (req, res) => {
      try {
           console.log(req.query);
-          const {category,stars} = req.query;
+          const { category, stars } = req.query;
           // const stars = req.query.stars;
           const filters = {};
           if (category) {
@@ -16,7 +17,7 @@ const getAllBooks = async (req, res) => {
                filters.stars = parseInt(stars)
           }
           console.log(filters);
-          
+
           const allBooks = await books.find(filters);
           // console.log(allBooks);
           res.status(200).json({ allBooks });
@@ -38,77 +39,78 @@ const getOneBook = async (req, res) => {
           }
      } catch (e) {
           console.error(e);
-          res.status(500).json({msg:e});
+          res.status(500).json({ msg: e });
      }
 };
+
 
 const createBook = async (req, res) => {
      try {
 
-     //    console.log(req.file);
-     //    console.log(req.body);
-        
-        const newBook = await books.create({
-          title: req.body.title,
-          slug: req.body.slug,
-          stars: req.body.stars,
-          description: req.body.description,
-          category: req.body.category,
-          createdAt:req.body.createdAt,
-          thumbnail:req.file.filename
-        });
+          //    console.log(req.file);
+          //    console.log(req.body);
+          const cloud_res = await cloudinary.uploader.upload(`/tmp/${req.file.filename}`);
+          console.log(cloud_res);
+          const newBook = await books.create({
+               title: req.body.title,
+               slug: req.body.slug,
+               stars: req.body.stars,
+               description: req.body.description,
+               category: req.body.category,
+               createdAt: req.body.createdAt,
+               thumbnail: req.file.filename
+          });
 
-        
 
-        console.log(newBook);
-        res.status(200).json({ success: true, book: newBook });
+          console.log(newBook);
+          res.status(200).json({ success: true, book: newBook });
      } catch (error) {
-        console.error(error);
-        res.status(500).json({ success: false, msg: String (error )});
+          console.error(error);
+          res.status(500).json({ success: false, msg: String(error) });
      }
-  };
-  
-const updateBook=async(req,res)=>{
-     try {
-          const {id:bookId}=req.params;
+};
 
-          const updated={
-               title:req.body.title,
-               slug:req.body.slug,
-               description:req.body.description,
-               category:req.body.category,
-               createdAt:req.body.createdAt,
-               stars:req.body.stars,
+const updateBook = async (req, res) => {
+     try {
+          const { id: bookId } = req.params;
+
+          const updated = {
+               title: req.body.title,
+               slug: req.body.slug,
+               description: req.body.description,
+               category: req.body.category,
+               createdAt: req.body.createdAt,
+               stars: req.body.stars,
           }
           if (req.file) {
-               updated.thumbnail=req.file.filename
+               updated.thumbnail = req.file.filename
           }
-          const updatedbook=await books.findByIdAndUpdate(bookId,updated,{
-               new:true,
-               runValidators:true,
+          const updatedbook = await books.findByIdAndUpdate(bookId, updated, {
+               new: true,
+               runValidators: true,
           });
 
           console.log(updateBook);
-          res.status(200).json({success:true,updatedbook:updatedbook})
+          res.status(200).json({ success: true, updatedbook: updatedbook })
      } catch (error) {
           console.log(error);
           res.status(500).json({ success: false, msg: error });
      }
 }
 
-const deleteBook=async(req,res)=>{
+const deleteBook = async (req, res) => {
      try {
 
-          const {id:bookId}=req.params;
+          const { id: bookId } = req.params;
           console.log(bookId);
-          const book=await books.findById(bookId);
-          const thumbnail=book._doc.thumbnail;
-          const filePath=`./tmp/${thumbnail}`
-          fs.unlinkSync(path.join(__dirname,"../",filePath));
-          const deleted=await books.findByIdAndDelete(bookId)
+          const book = await books.findById(bookId);
+          const thumbnail = book._doc.thumbnail;
+          const filePath = `./tmp/${thumbnail}`
+          fs.unlinkSync(path.join(__dirname, "../", filePath));
+          const deleted = await books.findByIdAndDelete(bookId)
 
           console.log(deleted);
-          res.status(200).json({success:true,msg:"Book Deleted successfully",deleteBook:deleted})
+          res.status(200).json({ success: true, msg: "Book Deleted successfully", deleteBook: deleted })
 
      } catch (error) {
           console.log(error);
@@ -116,12 +118,12 @@ const deleteBook=async(req,res)=>{
      }
 }
 
-const test=async(req,res)=>{
+const test = async (req, res) => {
      try {
           console.log(__dirname);
-          res.status(200).json({success:true})
+          res.status(200).json({ success: true })
      } catch (error) {
           console.log(error);
      }
 }
-module.exports = { getAllBooks, getOneBook ,createBook,updateBook,deleteBook,test};
+module.exports = { getAllBooks, getOneBook, createBook, updateBook, deleteBook, test };
