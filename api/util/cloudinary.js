095570c7a1a2c951476b53314fce1bec37f2bbf5
Mohd Abuzaid Ansari <mohdabuzaid15@gmail.com>
2023-12-30 16:36:18 +0530

    chngs

---
+++ api/util/cloudinary.js
@@ -0,0 +1,8 @@
+import {v2 as cloudinary} from 'cloudinary';
+          
+cloudinary.config({ 
+  cloud_name: 'dt7crhnpm', 
+  api_key: '784155382655383', 
+  api_secret: 'hLJMXPqx7fU1d_fIMtOebSpM4ss' 
+});
+module.exports=cloudinary
